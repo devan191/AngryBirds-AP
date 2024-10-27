5acf5cf8f89b977760b27f43a7dd2fc548b29216
Sahitya0411 <142541104+Sahitya0411@users.noreply.github.com>
2024-10-27 15:01:55 +0530

    Add files via upload

---
+++ core/src/main/java/com/de1/AngryBirds/GameObjects/Blocks/Wood.java
@@ -0,0 +1,9 @@
+package com.de1.AngryBirds.GameObjects.Blocks;
+
+public class Wood extends Block{
+
+    public Wood(float xPos, float yPos){
+        super(xPos, yPos);
+        this.health = 200;
+    }
+}
