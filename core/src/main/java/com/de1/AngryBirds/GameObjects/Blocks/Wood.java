b105ac00e7ca554a4183d05e6403b5c3752d4619
Sahitya0411 <142541104+Sahitya0411@users.noreply.github.com>
2024-11-29 21:57:18 +0530

    Update Wood.java

--- core/src/main/java/com/de1/AngryBirds/GameObjects/Blocks/Wood.java
+++ core/src/main/java/com/de1/AngryBirds/GameObjects/Blocks/Wood.java
@@ -1,9 +1,11 @@
 package com.de1.AngryBirds.GameObjects.Blocks;
 
+import com.badlogic.gdx.physics.box2d.World;
+
 public class Wood extends Block{
 
-    public Wood(float xPos, float yPos){
-        super(xPos, yPos);
-        this.health = 200;
+    public Wood(World world ,float xPos, float yPos, int health){
+        super(world,xPos, yPos);
+        this.health = health;
     }
 }
